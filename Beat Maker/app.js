bd19cd03666c64926c5d12e1edc362fa61fc5c0c
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2021-04-10 14:52:41 +0200

    Add files via upload

---
+++ Beat Maker/app.js
@@ -0,0 +1,54 @@
+var audio = new Audio();
+var counter = 0;
+var seconds = 0.4 * 1000;
+var interval;
+
+var output = document.getElementById("output");
+var playButton = document.getElementById("play-button");
+
+function start(status) {
+  if (status) {
+    interval = setInterval(playBeatMaker, seconds);
+    console.log(seconds);
+    playButton.disabled = true;
+  } else {
+    clearInterval(interval);
+    output.innerHTML = "Index: " + 0;
+    counter = 0;
+    playButton.disabled = false;
+  }
+}
+
+function playBeatMaker() {
+  counter++;
+  var checkboxUp = document.querySelector(`.row-1 input:nth-child(${counter})`);
+  var checkboxMiddle = document.querySelector(`.row-2 input:nth-child(${counter})`);
+  var checkboxDown = document.querySelector(`.row-3 input:nth-child(${counter})`);
+  output.innerHTML = "Index: " + counter;
+
+  if (counter == 8) {
+    counter = 0;
+  }
+
+  if (checkboxUp.checked) {
+    audio.src = "media/kick-slapback.wav";
+    audio.play();
+  }
+  if (checkboxMiddle.checked) {
+    audio.src = "media/kick-electro01.wav";
+    audio.play();
+  }
+  if (checkboxDown.checked) {
+    audio.src = "media/snare-electro.wav";
+    audio.play();
+  }
+}
+
+function beatIntervalSeconds(e) {
+  var inputValue = e.target.value;
+  if (inputValue != "" || inputValue > -1) {
+    seconds = inputValue * 1000;
+  } else {
+    seconds = 0.4 * 1000;
+  }
+}
